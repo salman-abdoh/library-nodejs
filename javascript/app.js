2fe552984e29d4f1e06dd2677d44944704d22226
salman-abdoh <80646924+salman-abdoh@users.noreply.github.com>
2022-01-27 03:59:17 +0300

    change in font and line and word on file css

--- javascript/app.js
+++ javascript/app.js
@@ -125,3 +125,43 @@ success.style.display="block"
             localStorage.setItem("cookicount" ,cookicount);
             cookicount=localStorage.getItem("cookicount");
         }
+        function langChange()
+        {
+            var styles = document.getElementsByTagName('link');
+            var en=document.getElementById('css');
+            if(en.getAttribute('href') =='css/style.css'){
+              en.setAttribute('href', 'css/en.css');
+            }
+            else{
+               en.setAttribute('href', 'css/style.css');
+            }
+        }
+        /******slid */
+        var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
